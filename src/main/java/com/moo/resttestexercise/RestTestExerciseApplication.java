ede19056fdbf196f8fd906b97b7e3cc45f7ce2bc
Robert Gruber <gruber.robert@gmail.com>
2020-02-19 15:22:20 +0000

    initial spring boot project structure

---
+++ src/main/java/com/moo/resttestexercise/RestTestExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.moo.resttestexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTestExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTestExerciseApplication.class, args);
+	}
+
+}
